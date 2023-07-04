83baa0d83754543435a668b3f3ee24bb63663e70
Michael Cacciatore <mcacciatore116@gmail.com>
2023-07-04 16:21:04 -0400

    Update privacy.js

--- src/pages/reddit/privacy.js
+++ src/pages/reddit/privacy.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Layout from '../components/layout';
-import SEO from '../components/seo';
+import Layout from '../../components/layout';
+import SEO from '../../components/seo';
 
 const __html = `<style>
   [data-custom-class='body'], [data-custom-class='body'] * {
