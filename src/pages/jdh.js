6b80211e5ea1516117a6265288b6df92b7b3af4f
Michael Cacciatore <mcacciatore116@gmail.com>
2025-02-24 21:55:40 -0500

    Create jdh.js

---
+++ src/pages/jdh.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const RedditPage = () => (
+  <Layout>
+    <SEO title="Joy's Dance House App" />
+    <h1>Joy&apos;s Dance House</h1>
+    <p>
+      The Joys Dance House application is a way for parents and teachers to connect. Most importantly, it allows the faculty and staff of Joys Dance House to send important announcements to parents.
+    </p>
+    <p>For support, please reach out to Ms. Brielle (or Mr. Brielle)</p>
+  </Layout>
+);
+
+export default RedditPage;
